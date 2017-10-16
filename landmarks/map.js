e8f9d3ce373be5bcc5c1895391b628f8aaeda179
pash01 <PaulinaAsh@Paulinas-MacBook-Air-2.local>
2017-10-15 20:25:03 -0400

    added comments to map.js file and cleaned up code

--- landmarks/map.js
+++ landmarks/map.js
@@ -4,8 +4,8 @@ var myLng;
 var myLogin = "CzDLNpQw";
 
 var closest_landmark_position;
-var closest_landmark_distance = 1000000;
-var closest_landmark_title = "";
+var closest_landmark_distance = 1000000; // set initially to nonsense value
+var closest_landmark_title = ""; // set initially to nonsense value
 
 var request = new XMLHttpRequest();
 var url = "https://defense-in-derpth.herokuapp.com/sendLocation";
@@ -18,7 +18,7 @@ var myOptions = {
 	mapTypeId: google.maps.MapTypeId.ROADMAP
 };
 var my_marker;
-var infowindow = new google.maps.InfoWindow();
+var infowindow = new google.maps.InfoWindow(); // only info window created for entire page
 
 function init() {
 	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
@@ -60,7 +60,7 @@ function renderMap() {
 		icon: 'me_icon.png',
 		title: "Here I Am! The closest landmark to me is " + closest_landmark_title + " , and is " + closest_landmark_distance.toFixed(2) + " miles away."
 	});
-	my_marker.setMap(map);
+	my_marker.setMap(map); // put my marker on the map 
 
 	// create polyline between me and the closest landmark to me
 	var my_path = new google.maps.Polyline({
@@ -70,7 +70,7 @@ function renderMap() {
 		strokeOpacity: 1.0,
 		strokeWeight: 2
 	});
-	my_path.setMap(map);
+	my_path.setMap(map); // put polyline on the map 
 		
 	// Open my info window on click of marker
 	google.maps.event.addListener(my_marker, 'click', function() {
@@ -92,7 +92,7 @@ function populateMap(responseData) {
 		});
 		person_marker.setMap(map);
 
-		// Open info window on click of marker
+		// Open info window on click of person marker
 		google.maps.event.addListener(person_marker, 'click', function() {
 			infowindow.setContent(this.title);
 			infowindow.open(map, this);
@@ -103,6 +103,7 @@ function populateMap(responseData) {
 	for (var i = 0; i < responseData.landmarks.length; i++) {
 		var landmark_position = new google.maps.LatLng(responseData.landmarks[i].geometry.coordinates[1], 
 													   responseData.landmarks[i].geometry.coordinates[0]);
+		// compute landmark's distance from me, in miles
 		var landmark_distance = (google.maps.geometry.spherical.computeDistanceBetween(landmark_position, my_position)) * 0.000621371;
 
 		//find landmark closest to me
@@ -119,9 +120,9 @@ function populateMap(responseData) {
 				icon: 'landmark_icon.png',
 				title: responseData.landmarks[i].properties.Location_Name
 			});
-			landmark_marker.setMap(map);	
+			landmark_marker.setMap(map); // put lankmark on the map
 
-			// Open info window on click of marker
+			// Open info window on click of landmark marker
 			google.maps.event.addListener(landmark_marker, 'click', function() {
 				infowindow.setContent(this.title);
 				infowindow.open(map, this);
